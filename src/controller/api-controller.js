f538e49e96207cbfa47aaac6af1b91f23a9c2a74
davcop <info@dcopelli.it>
2018-07-13 10:18:10 +0200

    Route con Auth

--- src/controller/api-controller.js
+++ src/controller/api-controller.js
@@ -5,31 +5,37 @@ import Docexp from '../model/experience'
 import Docedu from '../model/education'
 
 
-export function checkAuthLogin(req,res) {
-  // CHIAVE SEGRETA: 123456789
-  let idtoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5OSIsIm5hbWUiOiJEYXZpZGUiLCJpYXQiOjE1MTYyMzkwMjJ9.OhVFKX2rdFf_QlSgiMwcLalLUOUvpGfamH9qpboqv9U';
-  let expire = new Date().getTime() + 300*1000;
+export function checkAuthLogin(req, res) {
+    // CHIAVE SEGRETA: 123456789
+    let idtoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDVkFQUCIsInVpZCI6Ijk5OSIsIm5hbWUiOiJEYXZpZGUgQ29wZWxsaSIsImlhdCI6MTUxNjIzOTAyMn0.-HWHh1kPHo639rDKQAZas_gS4Gs6pGpsTPfBOg3Jx8A';
+    let expire = new Date().getTime() + 300 * 1000;
 
-  const jwt = {
-      idToken: idtoken,
-      scadenza: expire
-  };
+    const jwt = {
+        idToken: idtoken,
+        scadenza: expire
+    };
 
-  if (req.body.username=='demo' && req.body.password=='demo') {
-    res.json(jwt);
-  } else  {
-    res.status(400).json({"msg": "Credenziali errate", "error": 1})
-  }
+    if (req.body.username == 'demo' && req.body.password == 'demo') {
+        res.json(jwt);
+    } else {
+        res.status(401).json({
+            "msg": "Credenziali errate",
+            "error": 1
+        })
+    }
 
 }
 
 // Mostro la lista delle esperienze
 
-export function showListExperience(req,res) {
+export function showListExperience(req, res) {
     //SELECT titolo, descrizione FROM experience
-    Docexp.find({}, 'mansione azienda descrizione periodo status', (err,listaexp)=>{
-        if(err) {
-            res.status(400).json({"msg": err, "error": 1})
+    Docexp.find({}, 'mansione azienda descrizione periodo status', (err, listaexp) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
         }
         res.json(listaexp);
     })
@@ -37,79 +43,256 @@ export function showListExperience(req,res) {
 
 // Aggiunta Experience
 
-export function addExperience(req,res) {
-const newexp = new Docexp({
-    mansione: req.body.mansione,
-    azienda: req.body.azienda,
-    descrizione: req.body.descrizione,
-    periodo: req.body.periodo,
-    status : req.body.status
-})
+export function addExperienceAuth(req, res) {
+    const newexp = new Docexp({
+        mansione: req.body.mansione,
+        azienda: req.body.azienda,
+        descrizione: req.body.descrizione,
+        periodo: req.body.periodo,
+        status: req.body.status
+    })
 
-newexp.save((err,exp)=>{
-    if (err) {
-        res.status(400).json({"msg": err, "error": 1})
+    if (req.body.payload) {
+        if (req.body.payload['uid'] == "999") {
+            newexp.save((err, exp) => {
+                if (err) {
+                    res.status(400).json({
+                        "msg": err,
+                        "error": 1
+                    })
+                }
+                res.json(exp);
+            })
+        } else {
+            res.status(400).json({
+                "msg": "Utente non verificato. Effettua il login",
+                "error": 1
+            })
+        }
+    } else {
+        res.status(400).json({
+            "msg": "Accesso non autorizzato. Effettua il login",
+            "error": 1
+        })
     }
-    res.json(exp);
-})
+
+}
+
+
+export function addExperience(req, res) {
+    const newexp = new Docexp({
+        mansione: req.body.mansione,
+        azienda: req.body.azienda,
+        descrizione: req.body.descrizione,
+        periodo: req.body.periodo,
+        status: req.body.status
+    })
+
+    newexp.save((err, exp) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
+        }
+        res.json(exp);
+    })
 
 }
 
 // Mostro singola esperienza
 
-export function showExperience(req,res) {
-     //SELECT titolo, descrizione FROM experience WHERE id=923834784
-    Docexp.findOne({_id: req.params.id}, 'mansione azienda descrizione periodo status', (err,exp)=>{
-        if(err) {
-            res.status(400).json({"msg": err, "error": 1})
+export function showExperience(req, res) {
+    //SELECT titolo, descrizione FROM experience WHERE id=923834784
+    Docexp.findOne({
+        _id: req.params.id
+    }, 'mansione azienda descrizione periodo status', (err, exp) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
         }
         res.json(exp);
     })
 
     //Docexp.find().limit().sort().select().exec()
 }
-export function updateExperience(req,res) {
+
+export function showExperienceAuth(req, res) {
+
+    if (req.body.payload) {
+        if (req.body.payload['uid'] == "999") {
+            //SELECT titolo, descrizione FROM experience WHERE id=923834784
+            Docexp.findOne({
+                _id: req.params.id
+            }, 'mansione azienda descrizione periodo status', (err, exp) => {
+                if (err) {
+                    res.status(400).json({
+                        "msg": err,
+                        "error": 1
+                    })
+                }
+                res.json(exp);
+            })
+        } else {
+            res.status(400).json({
+                "msg": "Utente non verificato. Effettua il login",
+                "error": 1
+            })
+        }
+    } else {
+        res.status(400).json({
+            "msg": "Accesso non autorizzato. Effettua il login",
+            "error": 1
+        })
+    }
+
+}
+
+//Docexp.find().limit().sort().select().exec()
+
+
+export function updateExperience(req, res) {
     // UPDATE tabella SET col=valore WHERE _id=
     const id = req.body._id;
     const utente = {
-      mansione: req.body.mansione,
-      azienda: req.body.azienda,
-      descrizione: req.body.descrizione,
-      periodo: req.body.periodo,
-      status: req.body.stato
+        mansione: req.body.mansione,
+        azienda: req.body.azienda,
+        descrizione: req.body.descrizione,
+        periodo: req.body.periodo,
+        status: req.body.stato
     }
-    Docexp.update({_id: id}, utente, (err,okobj)=>{
-      if(err) {
-          res.status(400).json({"msg": err, "error": 1})
-      }
-      if(okobj['nModified']) {
-        res.json(utente);
-      } else {
-        res.json(utente);  
-      }
-      
-      
-    })
-  }
-
-// Cancello experienza
-
-export function deleteExperience(req,res) {
-    // DELETE * FROM tabella WHERE id=
-      Docexp.remove({_id: req.body._id}, (err)=>{
-        if(err) {
-            res.status(400).json({"msg": err, "error": 1})
+    Docexp.update({
+        _id: id
+    }, utente, (err, okobj) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
         }
-        res.json({"msg":"Cancellazione avvenuta con successo", "error": 0});
+        if (okobj['nModified']) {
+            res.json(utente);
+        } else {
+            res.json(utente);
+        }
+
+
     })
 }
 
+export function updateExperienceAuth(req, res) {
+    // UPDATE tabella SET col=valore WHERE _id=
+    const id = req.body._id;
+    const utente = {
+        mansione: req.body.mansione,
+        azienda: req.body.azienda,
+        descrizione: req.body.descrizione,
+        periodo: req.body.periodo,
+        status: req.body.stato
+    }
 
-export function showListEducation(req,res) {
+    if (req.body.payload) {
+        if (req.body.payload['uid'] == "999") {    
+
+    Docexp.update({
+        _id: id
+    }, utente, (err, okobj) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
+        }
+        if (okobj['nModified']) {
+            res.json(utente);
+        } else {
+            res.json(utente);
+        }
+
+
+    })
+} else {
+    res.status(400).json({
+        "msg": "Utente non verificato. Effettua il login",
+        "error": 1
+    })
+}
+} else {
+res.status(400).json({
+    "msg": "Accesso non autorizzato. Effettua il login",
+    "error": 1
+})
+}
+
+}
+
+
+// Cancello experienza
+
+export function deleteExperience(req, res) {
+    // DELETE * FROM tabella WHERE id=
+    Docexp.remove({
+        _id: req.body._id
+    }, (err) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
+        }
+        res.json({
+            "msg": "Cancellazione avvenuta con successo",
+            "error": 0
+        });
+    })
+}
+
+export function deleteExperienceAuth(req, res) {
+
+    if (req.body.payload) {
+        if (req.body.payload['uid'] == "999") {   
+
+    // DELETE * FROM tabella WHERE id=
+    Docexp.remove({
+        _id: req.body._id
+    }, (err) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
+        }
+        res.json({
+            "msg": "Cancellazione avvenuta con successo",
+            "error": 0
+        });
+    })
+} else {
+    res.status(400).json({
+        "msg": "Utente non verificato. Effettua il login",
+        "error": 1
+    })
+}
+} else {
+res.status(400).json({
+    "msg": "Accesso non autorizzato. Effettua il login",
+    "error": 1
+})
+}
+
+}
+
+
+export function showListEducation(req, res) {
     //SELECT titolo, descrizione FROM experience
-    Docedu.find({}, 'scuola qualifica descrizione periodo votazione', (err,listaedu)=>{
-        if(err) {
-            res.status(400).json({"msg": err, "error": 1})
+    Docedu.find({}, 'scuola qualifica descrizione periodo votazione', (err, listaedu) => {
+        if (err) {
+            res.status(400).json({
+                "msg": err,
+                "error": 1
+            })
         }
         res.json(listaedu);
     })
--- src/routers/api-router.js
+++ src/routers/api-router.js
@@ -1,21 +1,65 @@
 import express from 'express'
 import path from 'path'
 import  * as cv from '../controller/api-controller';
+import jwt from 'jsonwebtoken';
 
 const apirouter = express.Router();
+
+// modifiche per JSON web Token
+// https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens#authenticating-and-creating-a-token
+// 1) npm install jsonwebtoken --save
+// 2) import jwt from 'jsonwebtoken';
 
 // Experience
 
 apirouter.get('/experience', cv.showListExperience)
+apirouter.get('/education/', cv.showListEducation)
+apirouter.post('/auth/login/', cv.checkAuthLogin)
 apirouter.post('/experience/add', cv.addExperience)
 apirouter.get('/experience/:id/show', cv.showExperience)
 apirouter.post('/experience/up', cv.updateExperience)
 apirouter.post('/experience/delete', cv.deleteExperience)
 
-apirouter.get('/education/', cv.showListEducation)
-
 // Autorizzazioni
-apirouter.post('/auth/login/', cv.checkAuthLogin)
+// Inserendole in questo punto, prima della definizione delle altre route,
+// tutte sono protette. E' uno dei tanti modi per proteggere delle route
+// in Node usando Express.
+
+apirouter.use(function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    //var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    let token = '';
+    if (req.headers.authorization) {
+      token = req.headers.authorization.split(' ')[1]; // Bearer TOKEN
+    }
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, '123456789', function(err, payload) {      
+          if (err) {
+            return res.json({ "success": false, "message": 'Autenticazione fallita per il Token', "error": 1 });    
+          } else {
+            // if everything is good, save to request for use in other routes
+            req.body.payload = payload;    
+            next();
+          }
+        });
+    
+      } else {
+    
+        // if there is no token
+        // return an error
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
+        });
+    
+      }
+    });
+
+apirouter.post('/experience-auth/add', cv.addExperienceAuth)
+apirouter.get('/experience-auth/:id/show', cv.showExperienceAuth)
+apirouter.post('/experience-auth/up', cv.updateExperienceAuth)
+apirouter.post('/experience-auth/delete', cv.deleteExperienceAuth)
 
 export {apirouter}
 
