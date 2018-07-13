93b2f2ec00f170bb7432365586c9216171ea515b
davcop <info@dcopelli.it>
2018-07-13 17:45:37 +0200

    Error

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
