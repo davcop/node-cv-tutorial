dd7960ff2bfd57712a1614d4323921e723bf4dc5
davcop <info@dcopelli.it>
2018-07-13 17:10:16 +0200

    Added Access Control Headers

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
 
